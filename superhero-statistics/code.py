3de15fd0e8422b4ad42483a6e6d5b49664a008d6
Harish Bandapalli <bandapalliharish155@gmail.com>
2018-12-15 12:08:48 +0530

    Code file created.

---
+++ superhero-statistics/code.py
@@ -0,0 +1,72 @@
+# --------------
+#Header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#path of the data file- path
+data = pd.read_csv(path)
+data['Gender'].replace('-', 'Agender', inplace=True)
+gender_count = data['Gender'].value_counts()
+gender_count.plot(kind='bar')
+plt.xlabel('Gender')
+plt.ylabel('Frequency')
+plt.show()
+#Code starts here 
+
+
+
+
+# --------------
+#Code starts here
+
+alignment = data['Alignment'].value_counts()
+alignment.plot(kind = 'pie', autopct='%1.1f%%', legend=True, title='Charecter Alignment')
+plt.axis('equal')
+plt.show()
+
+
+# --------------
+#Code starts here
+sc_df = data[['Strength','Combat']].copy()
+
+sc_covariance = sc_df.cov().loc['Strength', 'Combat']
+sc_strength = sc_df['Strength'].std()
+sc_combat = sc_df['Combat'].std()
+
+sc_pearson = sc_covariance / (sc_strength * sc_combat)
+
+ic_df = data[['Intelligence','Combat']].copy()
+
+ic_covariance = ic_df.cov().loc['Intelligence', 'Combat']
+ic_intelligence = ic_df['Intelligence'].std()
+ic_combat = ic_df['Combat'].std()
+
+ic_pearson = ic_covariance / (ic_intelligence * ic_combat)
+
+
+# --------------
+#Code starts here
+
+total_high = data['Total'].quantile(.99)
+super_best = data[data['Total'] > total_high]
+super_best_names = list(super_best['Name'])
+print(super_best_names)
+
+
+# --------------
+#Code starts here
+
+fig, (ax_1, ax_2, ax_3) = plt.subplots(1, 3)
+ax_1.boxplot(data['Intelligence'])
+ax_1.set_xlabel('Intelligence')
+ax_2.boxplot(data['Speed'])
+ax_2.set_xlabel('Speed')
+ax_3.boxplot(data['Power'])
+ax_3.set_xlabel('Power')
+fig.subplots_adjust(left=0.08, right=0.98, bottom=0.05, top=0.9,
+                    hspace=0.4, wspace=0.3)
+plt.show()
+
+
